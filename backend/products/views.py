70d47cba16f99040d5477e8dad4d12dfc5b4e035
benilyxdd <benilyxdd@gmail.com>
2021-08-17 22:47:54 +0800

    backend: update products view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Product
+from .serializers import ProductSerializers
 
 # Create your views here.
+
+def product_list(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializers(products, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ProductSerializers(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
