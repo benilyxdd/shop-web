be6539e99b00b5a46f744e1761ae58ceec1f5d5d
benilyxdd <benilyxdd@gmail.com>
2021-08-18 00:37:25 +0800

    backend: change json format response to api response

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,45 +3,50 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Product
 from .serializers import ProductSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
+@api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == "GET":
         products = Product.objects.all()
         serializer = ProductSerializers(products, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ProductSerializers(data=data)
+        # data = JSONParser().parse(request)
+        serializer = ProductSerializers(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, primary_key):
     try:
         product = Product.objects.get(pk=primary_key)
     except Product.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = ProductSerializers(product)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ProductSerializers(product, data=data)
+        # data = JSONParser().parse(request)
+        serializer = ProductSerializers(product, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         product.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
