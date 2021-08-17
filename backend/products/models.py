ad96b245f22719732a43048033e956696f0a570f
benilyxdd <benilyxdd@gmail.com>
2021-08-17 22:02:29 +0800

    add date created and last modified to products model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     cost = models.DecimalField(max_digits=6, decimal_places=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_created=True)
 
     def __str__(self) -> str:
         return self.name
