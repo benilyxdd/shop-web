ad50a6833ab28dad22b14af1dff718184bfbc035
benilyxdd <benilyxdd@gmail.com>
2021-08-18 18:26:02 +0800

    backend: auto create token when account created

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 # Create your models here.
 
 
@@ -18,7 +22,7 @@ class MyAccountManager(BaseUserManager):
 
     def create_superuser(self, email, username, password):
         user = self.create_user(email=self.normalize_email(email),
-                          username=username, password=password)
+                                username=username, password=password)
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
@@ -50,3 +54,9 @@ class Account(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
