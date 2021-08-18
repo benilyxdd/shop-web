d4897f2931889a28fdb661cc032ed2d365889962
benilyxdd <benilyxdd@gmail.com>
2021-08-19 01:26:39 +0800

    frontend: make it more clear

--- frontend/src/component/ProductBlock/ProductBlock.js
+++ frontend/src/component/ProductBlock/ProductBlock.js
@@ -4,21 +4,12 @@ import "./ProductBlock.css";
 
 const ProductBlock = (props) => {
 	const prop = props.Product;
-	const Image = () => {
-		return <img src={prop.image} alt="stone"></img>;
-	};
-	const Name = () => {
-		return <h3>{prop.name}</h3>;
-	};
-	const Price = () => {
-		return <h3>$ {prop.price}</h3>;
-	};
 	// console.log(prop);
 	return (
 		<div className="product-display">
-			<Image />
-			<Name />
-			<Price />
+			<img src={prop.image} alt="stone" />
+			<h3>{prop.name}</h3>
+			<h3>$ {prop.price}</h3>
 		</div>
 	);
 };
