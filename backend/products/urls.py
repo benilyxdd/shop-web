b2ec49daf714de694f3793736bc5d56c00342ae2
benilyxdd <benilyxdd@gmail.com>
2021-08-18 14:55:55 +0800

    backend: add generic api view to product model

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import product_list, product_detail, ProductAPIView, ProductDetailAPIView
+from .views import product_list, product_detail, ProductAPIView, ProductDetailAPIView, ProductGenericAPIView
 
 urlpatterns = [
     # path('product/', product_list),
-    path('product/', ProductAPIView.as_view()),
+    # path('product/', ProductAPIView.as_view()),
+    path('product/', ProductGenericAPIView.as_view()),
     # path('product/detail/<int:primary_key>', product_detail),
-    path('product/detail/<int:id>', ProductDetailAPIView.as_view()),
+    path('product/detail/<int:id>', ProductGenericAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -5,10 +5,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import mixins, generics
 from .models import Product
 from .serializers import ProductSerializers
 
+
 # Create your views here.
+
+class ProductGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ProductSerializers
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+    
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 
 class ProductAPIView(APIView):
