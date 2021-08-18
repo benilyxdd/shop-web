da7bcb03edbb387901b70b19da4ace6a9f2af2fa
benilyxdd <benilyxdd@gmail.com>
2021-08-19 01:25:02 +0800

    frontend: add more information to product details page

--- frontend/src/component/ProductDetail/ProductDetail.js
+++ frontend/src/component/ProductDetail/ProductDetail.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./ProductDetail.css";
 
 const ProductDetail = (props) => {
 	const { id } = useParams();
+	const selectedProduct = useSelector((state) => state.initial.products)[
+		id - 1
+	];
 
 	return (
 		<div>
-			<h1>ProductDetail {id}</h1>
+			<h1>{selectedProduct.name}</h1>
+			<img src={selectedProduct.image} alt="stone" />
+			<h2>$ {selectedProduct.price}</h2>
 		</div>
 	);
 };
