5982a297fa81a5c6d4e1b633a4cd3afb8dde87fb
skywcs <skywcs@connect.hku.hk>
2021-08-20 10:56:15 +0800

    done product bar
    Co-authored-by: benily <benilyxdd@users.noreply.github.com>

--- frontend/src/component/ProductBar/ProductBar.js
+++ frontend/src/component/ProductBar/ProductBar.js
@@ -10,16 +10,25 @@ import "./ProductBar.css";
 // 原石
 
 const ProductBar = () => {
-	return (
-		<div className="ProductList">
-			<button>水晶手串</button>
-			<button>水晶簇</button>
-			<button>水晶球</button>
-			<button>水晶柱</button>
-			<button>吊墜</button>
-			<button>原石</button>
-		</div>
-	);
+    const handleClick = (e) => {
+        console.log(e.target.value);
+    };
+
+    return (
+        <div
+            className="ProductList"
+            onClick={(e) => {
+                handleClick(e);
+            }}
+        >
+            <button value="水晶手串">水晶手串</button>
+            <button value="水晶簇">水晶簇</button>
+            <button value="水晶球">水晶球</button>
+            <button value="水晶柱">水晶柱</button>
+            <button value="吊墜">吊墜</button>
+            <button value="原石">原石</button>
+        </div>
+    );
 };
 
 export default ProductBar;
