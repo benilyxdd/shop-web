dd781522926da50b3f82aa5a4f067e3cd24f81a1
skywcs <skywcs@connect.hku.hk>
2021-08-20 11:25:12 +0800

    done filter reducer

--- frontend/src/store/Reducers/initial.js
+++ frontend/src/store/Reducers/initial.js
@@ -1,17 +1,19 @@
 import { FETCH_DATA } from "../Actions/initial";
 
 const initState = {
-	products: [],
-	filteredProducts: []
+    products: [],
+    filteredProducts: [],
 };
 
 const initialReducer = (state = initState, action) => {
-	switch (action.type) {
-		case FETCH_DATA:
-			return { ...state, products: action.data };
-		default:
-			return state;
-	}
+    switch (action.type) {
+        case FETCH_DATA:
+            return { ...state, products: action.data };
+        case FILTER:
+            return { ...state, products: action.data };
+        default:
+            return state;
+    }
 };
 
 export default initialReducer;
